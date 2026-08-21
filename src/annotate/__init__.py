cf64ddb357aa49656702dc60182e67222203f179
Adam Karpierz <akarpierz@gmail.com>
2026-08-21 23:28:18 +0200

    Update documentation

--- src/annotate/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2012 Adam Karpierz
-# SPDX-License-Identifier: Zlib
-
-from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
-
-from ._annotate import * ; del _annotate  # type: ignore[name-defined]  # noqa
